5afe31d1e8182d8ab2485fda790ac6916e75b1d1
Sean Duffy <sean.the.duffy@gmail.com>
2018-03-09 18:30:56 -0500

    Timer implemented, Folder structure with placeholder assets added

---
+++ LockPicker/Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour 
+{
+	public enum Difficulty
+	{
+		EASY = 0,
+		MEDIUM,
+		HARD
+	}
+
+	public int playerSkill = 0;
+
+
+	void Start () 
+	{
+		
+	}
+	
+
+	void Update () 
+	{
+		
+	}
+
+	public static void GameOver()
+	{
+		Debug.Log ("GAME OVER");
+	}
+}
---
+++ LockPicker/Assets/Scripts/Timer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour 
+{
+	public Text timeText = null;
+
+	[SerializeField]
+	private float totalTime = 60.0f;
+
+	void Start () 
+	{
+		timeText.text = totalTime.ToString ();
+	}
+	
+
+	void Update () 
+	{
+		if(totalTime > 0.0f)
+		{
+			totalTime -= Time.deltaTime;
+			timeText.text = "Time Remaining: " + totalTime.ToString ("N0");
+		}
+
+		if(totalTime <= 0.0f)
+		{
+			GameManager.GameOver ();
+		}
+	}
+}
